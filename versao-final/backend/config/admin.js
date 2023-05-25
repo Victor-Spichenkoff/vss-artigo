d8af2d252810c97922113a9412b69741c5ce0195
Victor Spichenkoff Santana <victorspich13@gmail.com>
2023-05-25 14:50:31 -0300

    Finished

--- versao-final/backend/config/admin.js
+++
@@ -1,9 +0,0 @@
-module.exports = middleware => {
-    return (req, res, next) => {
-        if(req.user.admin) {
-            middleware(req, res, next)
-        } else {
-            res.status(401).send('Usuário não é administrador.')
-        }
-    }
-}
